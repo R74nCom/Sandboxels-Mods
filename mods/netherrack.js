79bad230ea768ad79485e00e3dd9ef6c00cd134d
somedevnamedtyler <discordtyler467@gmail.com>
2026-05-05 13:58:34 -0400

    Create netherrack.js
    from minecraft

---
+++ mods/netherrack.js
@@ -0,0 +1,10 @@
+elements.netherrack = {
+    color: "#722525",
+    behavior: behaviors.WALL,
+    category: "solids",
+    tempHigh: 1200,
+    stateHigh: "magma",
+    burn: 100,
+    burnTime: Infinity,
+    flammable: true
+};
