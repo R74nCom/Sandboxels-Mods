f20941430d500b2f4dd53837afcb8bf20dd086e1
plourdc-ctrl <plourdc@newhtfd.org>
2026-09-02 10:27:04 -0400

    Update kid_friendly.js

--- mods/kid_friendly.js
+++ mods/kid_friendly.js
@@ -7,15 +7,33 @@ delete elements.blood_2;
 delete elements.blood_particle;
 
 // Weapons / weapon-related elements
-delete elements.gun;
-delete elements.bullet;
-delete elements.bullet_casing;
-delete elements.rocket;
-delete elements.missile;
-delete elements.grenade;
 delete elements.bomb;
+delete elements.tnt;
+delete elements.C-4;
+delete elements.grenade;
+delete elements.dynamite;
+delete elements.gunpowder;
+delete elements.firework;
 delete elements.nuke;
-delete elements.firearm;
+delete elements.hbomb;
+delete elements.dirtybomb;
+delete elements.empbomb;
+delete elements.nitro
+delete elements.greekfire;
+delete elements.fireball;
+delete elements.rocket;
+delete elements.antibomb;
+delete elements.coldbomb;
+delete elements.hotbomb;
+delete elements.antimatterbomb;
+delete elements.partypopper;
+delete elements.flashbang;
+delete elements.smokegrenade;
+delete elements.landmine;
+delete elements.tornado;
+delete elements.earthquake;
+delete elements.tsunami;
+delete elements.blaster;
 
 // Prevent common explosive/weapon-like elements
 delete elements.explosion;
