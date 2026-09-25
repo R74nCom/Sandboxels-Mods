9439ca31b9ebcbf0087c789ab87164519c29b964
slweeb <91897291+slweeb@users.noreply.github.com>
2026-09-25 12:05:20 -0400

    Create centerFix.js

---
+++ mods/centerFix.js
@@ -0,0 +1,9 @@
+document.head.insertAdjacentHTML("beforeend", `<style>
+html.ad-layout-left #gameDiv {
+    left: 50%!important;
+    transform: translate(-50%, -0%)!important;
+}
+html.ad-layout-left .footer {
+    margin-left: auto!important;
+}
+</style>`);
